783ffa2b31c1bb637f4fe6cfd31663c721abe49b
Temuulen2830 <87805415+Temuulen2830@users.noreply.github.com>
2021-08-26 10:29:17 +1000

    Changed lines from 101 to 114
    Changed method and variable names from line 101 to 114 according to the code style guidelines.

--- src/library/borrowbook/BorrowBookUI.java
+++ src/library/borrowbook/BorrowBookUI.java
@@ -98,20 +98,20 @@ public class BorrowBookUI {
 				
 				
 			case COMPLETED:
-				OuTpUt("Borrowing Completed");
+				output("Borrowing Completed");							//Changed "OuTpUt" to output
 				return;
 	
 				
 			default:
-				OuTpUt("Unhandled state");
-				throw new RuntimeException("BorrowBookUI : unhandled state :" + StaTe);			
+				output("Unhandled state");							//Changed "OuTpUt" to output
+				throw new RuntimeException("BorrowBookUI : unhandled state :" + state);		//Changed "StaTe" to state
 			}
 		}		
 	}
 
 
-	public void DiSpLaY(Object object) {
-		OuTpUt(object);		
+	public void display(Object object) {			//Changed "DiSpLaY" to display
+		output(object);					//Changed "OuTpUt" to output
 	}
 
 
