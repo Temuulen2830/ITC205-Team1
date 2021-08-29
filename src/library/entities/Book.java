dd58e02a8ccc0895a13b7bbcd7c50fdde20ef60e
waduge340 <89051451+waduge340@users.noreply.github.com>
2021-08-29 22:03:48 +1000

    Update Book.java >> Rework due to a bad commit

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -5,62 +5,62 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Book implements Serializable {
 	
-	private String tItLe;
-	private String AuThOr;
-	private String CALLNO;
-	private int iD;
+	private String title;
+	private String author;
+	private String callNo;
+	private int id;
 	
-	private enum sTaTe { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
-	private sTaTe StAtE;
+	private enum BookState { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
+	private BookState state;
 	
 	
 	public Book(String author, String title, String callNo, int id) {
-		this.AuThOr = author;
-		this.tItLe = title;
-		this.CALLNO = callNo;
-		this.iD = id;
-		this.StAtE = sTaTe.AVAILABLE;
+		this.author = author;
+		this.title = title;
+		this.callNo = callNo;
+		this.id = id;
+		this.state = BookState.AVAILABLE;
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(iD).append("\n")
-		  .append("  Title:  ").append(tItLe).append("\n")
-		  .append("  Author: ").append(AuThOr).append("\n")
-		  .append("  CallNo: ").append(CALLNO).append("\n")
-		  .append("  State:  ").append(StAtE);
+		sb.append("Book: ").append(id).append("\n")
+		  .append("  Title:  ").append(title).append("\n")
+		  .append("  Author: ").append(author).append("\n")
+		  .append("  CallNo: ").append(callNo).append("\n")
+		  .append("  State:  ").append(state);
 		
 		return sb.toString();
 	}
 
-	public Integer gEtId() {
-		return iD;
+	public Integer getId() {
+		return id;
 	}
 
-	public String gEtTiTlE() {
-		return tItLe;
+	public String getTitle() {
+		return title;
 	}
 
 
 	
-	public boolean iS_AvAiLaBlE() {
-		return StAtE == sTaTe.AVAILABLE;
+	public boolean isAvailable() {
+		return state == BookState.AVAILABLE;
 	}
 
 	
-	public boolean iS_On_LoAn() {
-		return StAtE == sTaTe.ON_LOAN;
+	public boolean isOnLoan() {
+		return state == BookState.ON_LOAN;
 	}
 
 	
-	public boolean iS_DaMaGeD() {
-		return StAtE == sTaTe.DAMAGED;
+	public boolean isDamaged() {
+		return state == BookState.DAMAGED;
 	}
 
 	
-	public void BoRrOw() {
-		if (StAtE.equals(sTaTe.AVAILABLE)) 
-			StAtE = sTaTe.ON_LOAN;
+	public void borrowBook() {
+		if (state.equals(BookState.AVAILABLE)) 
+			state = BookState.ON_LOAN;
 		
 		else 
 			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", StAtE));
@@ -69,13 +69,13 @@ public class Book implements Serializable {
 	}
 
 
-	public void ReTuRn(boolean DaMaGeD) {
-		if (StAtE.equals(sTaTe.ON_LOAN)) 
+	public void returnBook(boolean DaMaGeD) {
+		if (state.equals(BookState.ON_LOAN)) 
 			if (DaMaGeD) 
-				StAtE = sTaTe.DAMAGED;
+				state = BookState.DAMAGED;
 			
 			else 
-				StAtE = sTaTe.AVAILABLE;
+				state = BookState.AVAILABLE;
 			
 		
 		else 
@@ -84,9 +84,9 @@ public class Book implements Serializable {
 	}
 
 	
-	public void RePaIr() {
-		if (StAtE.equals(sTaTe.DAMAGED)) 
-			StAtE = sTaTe.AVAILABLE;
+	public void repairBook() {
+		if (state.equals(BookState.DAMAGED)) 
+			state = BookState.AVAILABLE;
 		
 		else 
 			throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", StAtE));
